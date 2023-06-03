fe0baa162bf84e27f69bf704c79e91c8e46aa2ea
supermacchan <birkovska.dev@gmail.com>
2023-06-03 09:24:57 +0000

    Deploying to gh-pages from @ supermacchan/js-ninjas-superhero@8b7bfc9d4510f9b7207ad0d482d0e08efadf59f4 🚀

--- static/js/853.81e06098.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjs_ninjas_superhero=self.webpackChunkjs_ninjas_superhero||[]).push([[853],{3853:function(t,n,r){r.d(n,{Nte:function(){return o}});var e=r(9983);function o(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M12 20h9"}},{tag:"path",attr:{d:"M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"}}]})(t)}}}]);
-//# sourceMappingURL=853.81e06098.chunk.js.map
